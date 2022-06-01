5aa9de856d8fb0267adf70a26b4e2dc9ed343ada
shurikperunov@mail.ru <shurikperunov@mail.ru>
2022-06-01 14:16:58 +0500

    feat: реализовать добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+// 6 карточек при открытии страницы
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const page = document.querySelector('.page');
 const editButton = page.querySelector('.btn_type_edit');
 const popup = page.querySelector('.popup');
@@ -7,6 +35,29 @@ const profileJob = page.querySelector('.profile__job');
 const formElement = page.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.popup__input_type_name');
 const jobInput = formElement.querySelector('.popup__input_type_job');
+
+// карточки
+const cardsListElement = page.querySelector('.cards__list');
+const cardsTemplateElement = page.querySelector('.card-template');
+// console.log(cardsTemplateElement);
+
+function renderList(data) {
+  data.forEach(item => renderItem(item));
+}
+
+function renderItem(obj) {
+  // console.log("obj", obj.name);
+  const templateElement = page.querySelector('.card-template').content;
+  const listElement = templateElement.cloneNode(true);
+  // console.log('as', listElement);
+  const cardImgElement = listElement.querySelector('.card__img');
+  const cardPlaceElement = listElement.querySelector('.card__place');
+  cardImgElement.src = obj.link;
+  cardImgElement.alt = obj.name;
+  cardPlaceElement.textContent = obj.name;
+  // console.log('as', cardImgElement);
+  cardsListElement.append(listElement);
+}
 
 // открытие попап
 function openPopup() {
@@ -27,6 +78,8 @@ function formSubmitHandler(evt) {
   profileJob.textContent = jobInput.value;
   closePopup();
 }
+
+renderList(initialCards);
 
 editButton.addEventListener('click', openPopup)
 closeButton.addEventListener('click', closePopup);
