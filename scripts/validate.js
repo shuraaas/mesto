9e45698793750cb4c5929acb4a590ebcaa40f498
shurikperunov@mail.ru <shurikperunov@mail.ru>
2022-06-18 19:03:54 +0500

    refactor: сделать передачу объекта с настойками по всем функциям

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,4 @@
-const config = {
+const settings = {
   // селектор класса формы
   formSelector: '.form',
   // селектор поля ввода формы
@@ -13,44 +13,44 @@ const config = {
   errorClass: 'form__input-error_active'
 };
 
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(config.inputErrorClass);
+  inputElement.classList.add(settings.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add(config.errorClass);
+  errorElement.classList.add(settings.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, settings) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(config.inputErrorClass);
-  errorElement.classList.remove(config.errorClass);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.classList.remove(settings.errorClass);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, settings) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, settings);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, settings);
   }
 };
 
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-  const buttonList = Array.from(document.querySelectorAll(config.submitButtonSelector));
+const setEventListeners = (formElement, settings) => {
+  const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+  const buttonList = Array.from(document.querySelectorAll(settings.submitButtonSelector));
 
-  toggleButtonState(inputList, buttonList);
+  toggleButtonState(inputList, buttonList, settings);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonList);
+      checkInputValidity(formElement, inputElement, settings);
+      toggleButtonState(inputList, buttonList, settings);
     });
   });
 };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(config.formSelector));
+const enableValidation = (settings) => {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
 
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', function(evt) {
@@ -63,7 +63,7 @@ const enableValidation = () => {
     // });
 
     const fieldset = formElement.querySelector('.form__content');
-    setEventListeners(fieldset);
+    setEventListeners(fieldset, settings);
   });
 }
 
@@ -74,22 +74,20 @@ const hasInvalidInput = (inputList) => {
   });
 };
 
-const toggleButtonState = (inputList, buttonList) => {
+const toggleButtonState = (inputList, buttonList, settings) => {
 
 
   if(hasInvalidInput(inputList)) {
     buttonList.forEach((buttonElement) => {
-      buttonElement.classList.add(config.inactiveButtonClass);
+      buttonElement.classList.add(settings.inactiveButtonClass);
       buttonElement.disabled = true;
     });
   } else {
     buttonList.forEach((buttonElement) => {
-      buttonElement.classList.remove(config.inactiveButtonClass);
+      buttonElement.classList.remove(settings.inactiveButtonClass);
       buttonElement.disabled = false;
     });
   }
 };
 
-
-
-enableValidation();
+enableValidation(settings);
