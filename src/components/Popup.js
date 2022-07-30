10d84c2c35b8d73f1ef6bb8aa608c1ece0c55f05
shurikperunov@mail.ru <shurikperunov@mail.ru>
2022-07-31 00:23:19 +0500

    fix: забиндить метод класса в конструкторе

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,10 @@
+import { page } from '../utils/constants.js';
+
 // класс отвечает за открытие и закрытие попапа
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._boundHandleEscClose = this._handleEscClose.bind(this)
   }
 
   // закрытие попапа по Escape
@@ -14,13 +17,15 @@ export default class Popup {
   // открытие попапа
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    page.classList.add('page_no-scroll');
+    document.addEventListener('keydown', this._boundHandleEscClose);
   }
 
   // закрытие попапа
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    page.classList.remove('page_no-scroll');
+    document.removeEventListener('keydown', this._boundHandleEscClose);
   }
 
   // добавляем слушатель клика иконке закрытия попапа
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -71,5 +71,6 @@ export {
   formEdit,
   formAddCard,
   placeNameInput,
-  urlPlaceInput
+  urlPlaceInput,
+  page
 };
